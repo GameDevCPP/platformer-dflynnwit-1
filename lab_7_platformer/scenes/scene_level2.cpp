d2f1cedf3ff915bb1cab6d531394afc18a39e7e3
denfl <dflynn@wit.ie>
2023-11-20 08:59:12 +0000

    Working on win32. Added small todos

--- lab_7_platformer/scenes/scene_level2.cpp
+++ lab_7_platformer/scenes/scene_level2.cpp
@@ -21,7 +21,14 @@ void Level2Scene::Load() {
   {
     // *********************************
 
+      player = makeEntity();
+      player->setPosition(ls::getTilePosition(ls::findTiles(ls::START)[0]));
+      auto s = player->addComponent<ShapeComponent>();
+      s->setShape<sf::RectangleShape>(Vector2f(20.f, 30.f));
+      s->getShape().setFillColor(Color::Magenta);
+      s->getShape().setOrigin(Vector2f(10.f, 15.f));
 
+      player->addComponent<PlayerPhysicsComponent>(Vector2f(20.f, 30.f));
 
 
 
@@ -38,13 +45,20 @@ void Level2Scene::Load() {
                        Vector2f(0, 24));
     // *********************************
     // Add HurtComponent
+    enemy->addComponent<HurtComponent>();
 
     // Add ShapeComponent, Red 16.f Circle
+    auto s = enemy->addComponent<ShapeComponent>();
+    s->setShape<sf::CircleShape>(16.f);
+    s->getShape().setFillColor(Color::Red);
+    s->getShape().setOrigin(Vector2f(16.f, 16.f));
 
 
 
 
     // Add EnemyAIComponent
+    enemy->addComponent<EnemyAIComponent>();
+
 
     // *********************************
   }
@@ -61,21 +75,20 @@ void Level2Scene::Load() {
     turret->addComponent<EnemyTurretComponent>();
   }
 
-  // Add physics colliders to level tiles.
-  {
-    // *********************************
+    // Add physics colliders to level tiles.
+    {
+        auto walls = ls::findTiles(ls::WALL);
+        for (auto w : walls) {
+            auto pos = ls::getTilePosition(w);
+            pos += Vector2f(20.f, 20.f); //offset to center
+            auto e = makeEntity();
+            e->setPosition(pos);
+            e->addComponent<PhysicsComponent>(false, Vector2f(40.f, 40.f));
+        }
+    }
 
 
-
-
-
-
-
-
-    // *********************************
-  }
-
-  cout << " Scene 2 Load Done" << endl;
+    cout << " Scene 2 Load Done" << endl;
   setLoaded(true);
 }
 
